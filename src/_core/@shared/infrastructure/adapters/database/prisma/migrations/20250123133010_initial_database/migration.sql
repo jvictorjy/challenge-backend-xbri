6f5e1e559b8913a1f040b033da0ee300fe364577
Victor Ribeiro <jvictorjy@gmail.com>
2025-01-24 12:37:57 -0300

    test: configure unit tests

---
+++ src/_core/@shared/infrastructure/adapters/database/prisma/migrations/20250123133010_initial_database/migration.sql
@@ -0,0 +1,61 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PEDING', 'PAID', 'SENT', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('SELLER', 'BUYER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "user_type" "UserType" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tire" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "brand" VARCHAR(255) NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "quantity_available" INTEGER NOT NULL,
+    "seller_id" VARCHAR(50) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "tire_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order" (
+    "id" TEXT NOT NULL,
+    "tire_id" VARCHAR(50) NOT NULL,
+    "buyer_id" VARCHAR(50) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price_total" DOUBLE PRECISION NOT NULL,
+    "status" "Status" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "tire" ADD CONSTRAINT "tire_seller_id_fkey" FOREIGN KEY ("seller_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_buyer_id_fkey" FOREIGN KEY ("buyer_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order" ADD CONSTRAINT "order_tire_id_fkey" FOREIGN KEY ("tire_id") REFERENCES "tire"("id") ON DELETE CASCADE ON UPDATE CASCADE;
